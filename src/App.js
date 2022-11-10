e437f3ad9626c38af4a91ce58f1860e2093d1857
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-10 16:42:19 +0300

    list done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import Header from './components/Header'
 
 function App() {
   return (
-    <section className="todopp">
-      <Header />
+    <>
+      <section className="todopp">
+        <Header />
+        <Content />
 
-    </section>
+      </section>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+
+        <form>
+            <input className="new-todo" placeholder="What needs to be done?" autoFocus />
+        </form>
+
+    )
+}
+
+export default Form
